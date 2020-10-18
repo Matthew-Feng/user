3b4b8d640de45754a2c7dbe9170cc6d8628a3c02
Matthew-Feng <60952148+Matthew-Feng@users.noreply.github.com>
2020-10-19 09:28:10 +1100

    make address and user one to one

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,15 @@
 insert into ADDRESS(city,postcode,state,street) values('Beijing','1000','Chaoyang','any street');
 insert into ADDRESS(city,postcode,state,street) values('Sydney','2000','NSW','abc street');
-insert into ADDRESS(city,postcode,state,street) values('NewYork','3000','US','Vicv street');
+insert into ADDRESS(city,postcode,state,street) values('NewYork','3000','US','Vicv street1');
+insert into ADDRESS(city,postcode,state,street) values('NewYork','3000','US','Vicv street2');
+insert into ADDRESS(city,postcode,state,street) values('NewYork','3000','US','Vicv street3');
+insert into ADDRESS(city,postcode,state,street) values('NewYork','3000','US','Vicv street4');
+insert into ADDRESS(city,postcode,state,street) values('NewYork','3000','US','Vicv street5');
 
 insert into user_detail(first_name,last_name,gender,title,address_id) values('Henrry','Matt','Male','Mr',1);
-insert into user_detail(first_name,last_name,gender,title,address_id) values('Luke','Jon','Male','Mr',1);
-insert into user_detail(first_name,last_name,gender,title,address_id) values('Man','peter','Male','Mr',2);
-insert into user_detail(first_name,last_name,gender,title,address_id) values('John','Sm itg','Male','Mr',2);
-insert into user_detail(first_name,last_name,gender,title,address_id) values('Foo','Doo','Female','Ms',1);
-insert into user_detail(first_name,last_name,gender,title,address_id) values('Good','Smitg','Female','Ms',3);
-insert into user_detail(first_name,last_name,gender,title,address_id) values('Gates','Clinton','Male','Ms',3);
+insert into user_detail(first_name,last_name,gender,title,address_id) values('Luke','Jon','Male','Mr',2);
+insert into user_detail(first_name,last_name,gender,title,address_id) values('Man','peter','Male','Mr',3);
+insert into user_detail(first_name,last_name,gender,title,address_id) values('John','Sm itg','Male','Mr',4);
+insert into user_detail(first_name,last_name,gender,title,address_id) values('Foo','Doo','Female','Ms',5);
+insert into user_detail(first_name,last_name,gender,title,address_id) values('Good','Smitg','Female','Ms',6);
+insert into user_detail(first_name,last_name,gender,title,address_id) values('Gates','Clinton','Male','Ms',7);
